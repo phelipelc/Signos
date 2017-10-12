99bdb79f9fdac5c2b95d2a8c12b0db43fc16f448
phelipelc <phelipelcampos@hotmail.com>
2017-10-12 00:17:57 -0300

    Interpretação do signo digitado
    Interpretação do signo digitado

--- app/src/main/java/br/com/phelipe/mobile/signos/Principal.java
+++ app/src/main/java/br/com/phelipe/mobile/signos/Principal.java
@@ -1,12 +1,15 @@
 package br.com.phelipe.mobile.signos;
 
+import android.content.Intent;
 import  android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
+import android.widget.Button;
 public class Principal extends AppCompatActivity {
-private Spinner spinnerDia =null;
-private Spinner spinnerMes =null;
+    private Spinner spinnerDia = null;
+    private Spinner spinnerMes = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,6 +27,35 @@ private Spinner spinnerMes =null;
         adapter_mes.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinnerDia.setAdapter(adapter_dia);
         spinnerMes.setAdapter(adapter_mes);
+        Button enviar = (Button) findViewById(R.id.buttonEnviar);
+        enviar.setOnClickListener(new View.OnClickListener() {
 
+            public void onClick(View view) {
+                int posicaoDia =  spinnerDia.getSelectedItemPosition();
+                int posicaoMes = spinnerMes.getSelectedItemPosition();
+                posicaoDia++;
+                posicaoMes++;
+
+                InterpretadorSigno Interpretador = new InterpretadorSigno();
+                Signo signoresultado = Interpretador.Interpretar(posicaoDia,posicaoMes);
+
+                Bundle args = new Bundle();
+                args.putSerializable("resultado", signoresultado);
+
+                Intent intent = new Intent(Principal.this, Resultado.class);
+                intent.putExtra("signo",args);
+
+                startActivity(intent);
+
+            }
+
+        });
     }
 }
+
+
+
+
+
+
+
--- app/src/main/java/br/com/phelipe/mobile/signos/Resultado.java
+++ app/src/main/java/br/com/phelipe/mobile/signos/Resultado.java
@@ -1,7 +1,11 @@
 package br.com.phelipe.mobile.signos;
 
+import android.graphics.drawable.Drawable;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class Resultado extends AppCompatActivity {
 
@@ -9,5 +13,24 @@ public class Resultado extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_resultado);
+
+        Bundle args = getIntent().getBundleExtra("signo");
+
+        if(args!=null){
+
+        Signo signoRecebido = (Signo) args.getSerializable("resultado");
+
+            int imageResource = getResources().getIdentifier(signoRecebido.getImagem(),null, getPackageName());
+            Drawable res  = ContextCompat.getDrawable(getApplicationContext(), imageResource);
+            ImageView imagem_signo = (ImageView)findViewById(R.id.imageSigno);
+            imagem_signo.setImageDrawable(res);
+
+            TextView resultado = (TextView) findViewById(R.id.textSigno);
+            TextView datas = (TextView) findViewById(R.id.textData);
+
+            resultado.setText(signoRecebido.getNome());
+            datas.setText("de" + signoRecebido.getDiainicio() + "/"+ signoRecebido.getMesinicio() + "até" +signoRecebido.getDiaFim() +"/"+ signoRecebido.getMesfim());
+        }
+
     }
 }
